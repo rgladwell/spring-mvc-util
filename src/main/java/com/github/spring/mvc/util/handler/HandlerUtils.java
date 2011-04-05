87ae533934acda4f7c0ba19e9c151f8eb4a650e6
rgladwell <ricardo.gladwell@gmail.com>
2011-04-05 19:06:38 +0100

    Added Apache license information

--- src/main/java/com/github/spring/mvc/util/handler/HandlerUtils.java
+++ src/main/java/com/github/spring/mvc/util/handler/HandlerUtils.java
@@ -1,9 +1,24 @@
+/**
+ * Copyright 2011 Ricardo Gladwell
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.github.spring.mvc.util.handler;
 
 public class HandlerUtils {
 
     // Matching code copied from org.apache.catalina.core.ApplicationFilterFactory.matchFiltersURL
-    public static boolean servletUrlPatternMatch(String pattern, String uri) {       
+    public static boolean servletUrlPatternMatch(String pattern, String uri) {
         // Case 1 - Exact Match
         if (pattern.equals(uri))
             return (true);
@@ -29,7 +44,7 @@ public class HandlerUtils {
             if ((slash >= 0) && (period > slash)
                     && (period != uri.length() - 1)
                     && ((uri.length() - period)
-                    == (pattern.length() - 1))) {
+                            == (pattern.length() - 1))) {
                 return (pattern.regionMatches(2, uri, period + 1, pattern.length() - 2));
             }
         }
